62e685928b94b07d7ec871fad62df129ab596a90
Kayn <kayn.lai@yu-jet.com>
2026-07-15 17:00:54 +0800

    feat: api

--- app/Models/mysql/User.php
+++ app/Models/mysql/User.php
@@ -7,11 +7,13 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens, HasUuids;
 
     /**
      * The attributes that are mass assignable.
---
+++ app/Repositories/Eloquent/Interface/EloquentRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories\Eloquent\Interface;
+
+class EloquentRepositoryInterface
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
