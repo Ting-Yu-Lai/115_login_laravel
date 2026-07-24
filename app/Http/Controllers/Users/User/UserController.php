56d5a4e3992460f725a3b725775a99d6e979a5f2
Kayn <kayn.lai@yu-jet.com>
2026-07-24 16:57:57 +0800

    add User and  AuthController and request validation

---
+++ app/Http/Controllers/Users/User/UserController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Users\User;
+
+use App\Http\Controllers\Controller;
+use App\Services\User\UserService;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //
+}
